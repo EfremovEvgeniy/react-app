46aeb79f7986b7305e6629c1139b56a1c8319adf
Evgenii Efremov <johne32rus23@gmail.com>
2020-08-10 08:48:30 +0300

    commit after 9 lesson

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from './header.js'
+import Technologies from './technologies.js'
 
 const App = () => {
   return (
@@ -9,28 +11,6 @@ const App = () => {
       <Technologies />
     </div>
   );
-}
-
-const Header = () => {
-  return (
-    <div>
-      <a href="#">Home</a>
-      <a href="#">New feed</a>
-      <a href="#">New messages</a>
-    </div>
-  );
-}
-
-const Technologies = () => {
-  return (
-    <div>
-      <ul>
-        <li>css</li>
-        <li>html</li>
-        <li>js</li>
-      </ul>
-    </div>
-  )
 }
 
 export default App;
