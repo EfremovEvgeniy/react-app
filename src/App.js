4e0923f38a5cc8dfc9478291750eb2cb2bf4b5d4
Evgenii Efremov <johne32rus23@gmail.com>
2020-08-10 09:40:51 +0300

    commit after 11 lesson

--- src/App.js
+++ src/App.js
@@ -1,14 +1,41 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Header from './header.js'
-import Technologies from './technologies.js'
 
 const App = () => {
   return (
-    <div className="App">
-      <Header />
-      <Technologies />
+    <div className='app-wrapper'>
+      <header className='header'><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQRD9watd98GuxuplYkqW5OizlYfQa_Iy4_3g&usqp=CAU" alt=""/></header>
+      <nav className='nav'>
+        <div><a href="#">Profile</a></div>
+        <div><a href="#">Messages</a></div>
+        <div><a href="#">News</a></div>
+        <div><a href="#">Music</a></div>
+        <div><a href="#">Settings</a></div>
+      </nav>
+      <div className='content'>
+        <div>
+          <img src="https://blog.iese.edu/cities-challenges-and-management/files/2019/05/hong-kong-1990268_960_720.jpg" alt=""/>
+        </div>
+        <div>
+           ava + desc
+        </div>
+        <div>
+          My posts
+          <div>
+            New post
+          </div>
+          <div>
+            <div>
+              Post 1
+            </div>
+            <div>
+              Post 2
+            </div>
+          </div>
+        </div>
+      </div>
+
     </div>
   );
 }
