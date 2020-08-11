3ee21e6925aa8e8b5d175a1986ae127690e091d1
Evgenii Efremov <johne32rus23@gmail.com>
2020-08-11 10:12:53 +0300

    update styles for grid

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -4,28 +4,24 @@ import s from './Profile.module.scss'
 const Profile = () => {
     return (
         <main className={s.content}>
-            <div className={`${s.container} ${s.clearfix}`}>
-                <div className={s.ava}>
-                    <img src="https://avatarfiles.alphacoders.com/693/69306.jpg" alt=""/>
-                </div>
-                <div className={s.description}>
-                    <p>Мой профиль</p>
-                </div>
+            <div className={s.ava}>
+                <img src="https://avatarfiles.alphacoders.com/693/69306.jpg" alt=""/>
             </div>
-            <div className={s.container}>
-                <div className={s.title}>
-                    <p>My posts</p>
+            <div className={s.description}>
+                <p>Мой профиль</p>
+            </div>
+            <div className={s.title}>
+                <span>My posts</span>
+            </div>
+            <div className={s.new}>
+                New post
+            </div>
+            <div className={s.posts}>
+                <div className={s.post}>
+                    Post 1
                 </div>
-                <div className={s.new}>
-                    New post
-                </div>
-                <div className={s.posts}>
-                    <div className={s.post}>
-                        Post 1
-                    </div>
-                    <div className={s.post}>
-                        Post 2
-                    </div>
+                <div className={s.post}>
+                    Post 2
                 </div>
             </div>
         </main>
