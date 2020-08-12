c722d68523fe67d8183c9469597a0624055dd23b
Evgenii Efremov <johne32rus23@gmail.com>
2020-08-12 11:55:03 +0300

    add some styles

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,13 +4,15 @@ import styles from "./Header.module.scss";
 const Header = () => {
   return (
     <header className={styles.header}>
-      <a href="http://localhost:3000/">
-        <img
-          className={styles.logo}
-          src="https://cdn.worldvectorlogo.com/logos/react.svg"
-          alt=""
-        />
-      </a>
+      <div>
+        <a href="http://localhost:3000/">
+          <img
+            className={styles.logo}
+            src="https://cdn.worldvectorlogo.com/logos/react.svg"
+            alt=""
+          />
+        </a>
+      </div>
     </header>
   );
 };
