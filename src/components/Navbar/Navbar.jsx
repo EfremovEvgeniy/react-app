6204b6fdcc831e69f2f18c53085cdfeca26972ec
Evgenii Efremov <johne32rus23@gmail.com>
2020-08-18 13:45:00 +0300

    refactored profile

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,24 +2,22 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import styles from "./Navbar.module.scss";
 
+const MenuItem = (props) => {
+  return (
+    <div className={styles.menu}>
+      <NavLink to={props.path}>{props.title}</NavLink>
+    </div>
+  );
+};
+
 const Navbar = () => {
   return (
     <nav className={styles.nav}>
-      <div className={styles.menu}>
-        <NavLink to="/profile">Profile</NavLink>
-      </div>
-      <div className={styles.menu}>
-        <NavLink to="/dialogs">Dialogs</NavLink>
-      </div>
-      <div className={styles.menu}>
-        <NavLink to="/news">News</NavLink>
-      </div>
-      <div className={styles.menu}>
-        <NavLink to="/music">Music</NavLink>
-      </div>
-      <div className={styles.menu}>
-        <NavLink to="/settings">Settings</NavLink>
-      </div>
+      <MenuItem path="/profile" title="Profile" />
+      <MenuItem path="/dialogs" title="Dialogs" />
+      <MenuItem path="/news" title="News" />
+      <MenuItem path="/music" title="Music" />
+      <MenuItem path="/settings" title="Settings" />
     </nav>
   );
 };
