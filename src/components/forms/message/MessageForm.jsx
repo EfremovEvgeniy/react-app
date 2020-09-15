b4e1f515f9d90a8ea18853b33ec11c3d625e66b4
Evgenii Efremov <johne32rus23@gmail.com>
2020-09-15 08:59:41 +0300

    add validation for message form

--- src/components/forms/message/MessageForm.jsx
+++ src/components/forms/message/MessageForm.jsx
@@ -1,14 +1,22 @@
 import { reduxForm, Field } from "redux-form";
 import React from "react";
 import styles from "./../SimpleForm.module.scss";
+import {
+  requiredField,
+  maxLengthCreator,
+} from "../../../utils/validators/validators";
+import TextArea from "../../Common/FormsControls/TextArea";
+
+const maxLength10 = maxLengthCreator(10);
 
 const MessageForm = (props) => {
   return (
     <form onSubmit={props.handleSubmit} className={styles.new}>
       <Field
-        component="textarea"
+        component={TextArea}
         name="newMessageBody"
         placeholder="Put your message here"
+        validate={[requiredField, maxLength10]}
       />
       <button>
         <span>Send message</span>
