ce202238d34583e5be23fa4b6770bccf0a4ec045
Evgenii Efremov <johne32rus23@gmail.com>
2020-09-15 08:58:13 +0300

    add validation for post form

--- src/components/forms/post/PostForm.jsx
+++ src/components/forms/post/PostForm.jsx
@@ -1,14 +1,22 @@
 import { reduxForm, Field } from "redux-form";
 import React from "react";
 import styles from "./../SimpleForm.module.scss";
+import {
+  requiredField,
+  maxLengthCreator,
+} from "../../../utils/validators/validators";
+import TextArea from "../../Common/FormsControls/TextArea";
+
+const maxLength10 = maxLengthCreator(10);
 
 const PostForm = (props) => {
   return (
     <form onSubmit={props.handleSubmit} className={styles.new}>
       <Field
-        component="textarea"
+        component={TextArea}
         name="newPostBody"
         placeholder="Put your post here"
+        validate={[requiredField, maxLength10]}
       />
       <button>
         <span>Add Post</span>
